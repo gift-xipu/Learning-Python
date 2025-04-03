e31d545513a68c8ce9b2dd24b442afbc4040f2c9
Giftxipu <gift.xipu@gmail.com>
2025-04-03 21:24:19 +0200

    just changes

---
+++ Basics/file manipulation/Data Layer/handlers.py
@@ -0,0 +1,32 @@
+import csv
+
+#base class for all files
+#standard for all csv file
+class CSVHandler:
+    def __init__ (self, FILE_PATH):
+        self.FILE_PATH = FILE_PATH
+
+    def read_file():
+        pass
+    def write_file():
+        pass
+    def append_file(entry):
+        pass
+
+class StudentFileData(CSVHandler):
+    def __init__(self):
+        super().__init__("students.csv")
+
+    def get_student_by_id(student_id):
+        pass
+
+    def get_students_by_course(course_id):
+        pass
+
+    def add_student(student):
+        pass
+
+    def update_student(student_id):
+        pass
+
+
